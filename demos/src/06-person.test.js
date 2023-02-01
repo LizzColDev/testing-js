7b698157ccd4461b6efde7e05942eca0c1a77733
LizzColDev <lquintero28@live.com>
2023-02-01 16:23:53 -0500

    Unit Test, coverage report, docker installation and API project

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -2,12 +2,18 @@ const Person = require('./06-person');
 
 describe('Test for Person', () => {
   let person;
+  // AAA
+  // Arrange / Given // preparar
+
   beforeEach(() => {
     person = new Person('Nicolas', 45, 1.7);
   });
   test('should return down', () => {
+    // Arrange / Given // preparar
     person.weight = 45;
+    // Act / When // actuar
     const imc = person.calcIMC();
+    // Assert / Then // acertar
     expect(imc).toBe('down');
   });
   test('should return normal', () => {
